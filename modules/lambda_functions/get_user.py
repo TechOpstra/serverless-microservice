e5f081d047e5e1a5ffed943b5310ec43257361b5
Harish <107391575+8555936327@users.noreply.github.com>
2025-04-19 00:07:16 +0530

    Update get_user.py

--- modules/lambda_functions/get_user.py
+++ modules/lambda_functions/get_user.py
@@ -2,7 +2,7 @@
 import boto3
 
 def lambda_handler(event, context):
-    table_name = 'serverless_workshop_intro'
+    table_name = 'UserData'
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table(table_name)
 
