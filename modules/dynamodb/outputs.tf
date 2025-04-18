ecefbfbce638f392d65c865e8973b6d5537eb173
Harish <107391575+8555936327@users.noreply.github.com>
2025-04-18 17:31:28 +0530

    Update outputs.tf

--- modules/dynamodb/outputs.tf
+++ modules/dynamodb/outputs.tf
@@ -1,4 +1,8 @@
-# dynamodb/outputs.tf
+
 output "dynamodb_table_name" {
   value = aws_dynamodb_table.user_data.name
+}
+
+output "dynamodb_table_arn" {
+  value = aws_dynamodb_table.user_data.arn
 }
