9fa1c656304109c78ec95682e47e00cd682a16d0
Harish <107391575+8555936327@users.noreply.github.com>
2025-04-18 23:44:41 +0530

    Update main.tf

--- modules/api_gateway/main.tf
+++ modules/api_gateway/main.tf
@@ -42,3 +42,17 @@ resource "aws_api_gateway_integration" "add_user" {
   integration_http_method = "POST"
   uri                     = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/${var.add_user_lambda_arn}/invocations"
 }
+
+# Deployment
+resource "aws_api_gateway_deployment" "user_api_deployment" {
+  depends_on = [
+    aws_api_gateway_integration.get_user,
+    aws_api_gateway_integration.add_user
+  ]
+  rest_api_id = aws_api_gateway_rest_api.user_api.id
+  stage_name  = "prod"
+}
+
+output "api_gateway_url" {
+  value = aws_api_gateway_deployment.user_api_deployment.invoke_url
+}
