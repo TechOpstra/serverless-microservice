162b8689700d6f9e009899a306d820a0a3946e55
Harish <107391575+8555936327@users.noreply.github.com>
2025-04-18 17:15:57 +0530

    Update main.tf

--- modules/api_gateway/main.tf
+++ modules/api_gateway/main.tf
@@ -1,4 +1,3 @@
-# api_gateway/main.tf
 resource "aws_api_gateway_rest_api" "user_api" {
   name        = var.api_name
   description = "API Gateway for user data management"
@@ -10,6 +9,7 @@ resource "aws_api_gateway_resource" "user" {
   path_part   = "user"
 }
 
+# Method for retrieving user data (GET)
 resource "aws_api_gateway_method" "get_user" {
   rest_api_id   = aws_api_gateway_rest_api.user_api.id
   resource_id   = aws_api_gateway_resource.user.id
@@ -18,10 +18,27 @@ resource "aws_api_gateway_method" "get_user" {
 }
 
 resource "aws_api_gateway_integration" "get_user" {
-  rest_api_id = aws_api_gateway_rest_api.user_api.id
-  resource_id = aws_api_gateway_resource.user.id
-  http_method = aws_api_gateway_method.get_user.http_method
-  type        = "AWS_PROXY"
+  rest_api_id             = aws_api_gateway_rest_api.user_api.id
+  resource_id             = aws_api_gateway_resource.user.id
+  http_method             = aws_api_gateway_method.get_user.http_method
+  type                    = "AWS_PROXY"
   integration_http_method = "POST"
-  uri         = aws_lambda_function.get_user.invoke_arn
+  uri                     = aws_lambda_function.get_user.invoke_arn
+}
+
+# Method for adding user data (POST)
+resource "aws_api_gateway_method" "add_user" {
+  rest_api_id   = aws_api_gateway_rest_api.user_api.id
+  resource_id   = aws_api_gateway_resource.user.id
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "add_user" {
+  rest_api_id             = aws_api_gateway_rest_api.user_api.id
+  resource_id             = aws_api_gateway_resource.user.id
+  http_method             = aws_api_gateway_method.add_user.http_method
+  type                    = "AWS_PROXY"
+  integration_http_method = "POST"
+  uri                     = aws_lambda_function.add_user.invoke_arn
 }
