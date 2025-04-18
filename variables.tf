b8a8519b4cca24af26894714486d498e0dcf8106
Harish <107391575+8555936327@users.noreply.github.com>
2025-04-18 16:59:53 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,8 +1,9 @@
-# variables.tf
+
 variable "region" {
   description = "region for the aws"
   type        = string
   default     = "us-east-1"
+
 variable "dynamodb_table_name" {
   description = "The name of the DynamoDB table"
   type        = string
