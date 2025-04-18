6e10a249675a254c0b5bb2e87b1749c933e3ac50
Harish <107391575+8555936327@users.noreply.github.com>
2025-04-18 16:56:44 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,18 @@
-# outputs.tf
+
 output "dynamodb_table_name" {
   value = module.dynamodb.dynamodb_table_name
 }
 
+output "lambda_role_arn" {
+  value = module.iam.lambda_role_arn
+}
+
 output "add_user_lambda_arn" {
   value = module.lambda.add_user_lambda_arn
+}
+
+output "get_user_lambda_arn" {
+  value = module.lambda.get_user_lambda_arn
 }
 
 output "api_gateway_url" {
